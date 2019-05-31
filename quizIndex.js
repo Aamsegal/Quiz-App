9259e1ed1c29ed0daeda779cc3b182766cf47d83
Amsegal93 <Amsegal93@gmail.com>
2019-05-31 06:41:17 -0400

    Made html wireframe clickable. Both question list and napkin wire frame were added earlier

--- quizIndex.js
+++ quizIndex.js
@@ -1,64 +1,64 @@
 const questionAndAnswer = [
-    {id: cuid(), question: "What type of animal is a seahorse?", answer1:"Crustacean",
+    /*{question: "What type of animal is a seahorse?", answer1:"Crustacean",
     answer2:"Arachnid",answer3:"Fish", answer4:"Shell", correctAnswer:"Crustacean"},
 
-    {id: cuid(), question: "Which of the following dogs is the smallest", answer1:"Dachshund",
+    {question: "Which of the following dogs is the smallest", answer1:"Dachshund",
     answer2:"Poodle",answer3:"Pomeranian", answer4:"Chihuahua", correctAnswer:"Chihuahua"},
 
-    {id: cuid(), question: "What color are Zebras?", answer1:"White with black stripes",
+    {question: "What color are Zebras?", answer1:"White with black stripes",
     answer2:"Black with shite stripes",answer3:"Both of the above", answer4:"None of the above", correctAnswer:"Black with white stripes"},
 
-    {id: cuid(), question: "What existing bird has the largest wingspan", answer1:"Stork",
+    {question: "What existing bird has the largest wingspan", answer1:"Stork",
     answer2:"Swan",answer3:"Condor", answer4:"Albatross", correctAnswer:"Albatross"},
 
-    {id: cuid(), question: "Whats the biggest animal that has ever lived", answer1:"Blue whale",
+    {question: "Whats the biggest animal that has ever lived", answer1:"Blue whale",
     answer2:"African elephant",answer3:"Brontosaurus", answer4:"Spinosaurus", correctAnswer:"Blue whale"},
 
-    {id: cuid(), question: "What pets do more familes own", answer1:"Birds",
+    {question: "What pets do more familes own", answer1:"Birds",
     answer2:"Cats",answer3:"Dogs", answer4:"Horses", correctAnswer:"Cats"},
 
-    {id: cuid(), question: "What animal lives the longest", answer1:"Clam",
+    {question: "What animal lives the longest", answer1:"Clam",
     answer2:"Red sea urchin",answer3:"Galapagos tortois", answer4:"Rougheye rockfish", correctAnswer:"Clam"},
 
-    {id: cuid(), question: "What are female elephants called?", answer1:"Mares",
-    answer2:"Sows",answer3:"Cows", answer4:"Dams", correctAnswer:"Cows"},
+    {question: "What are female elephants called?", answer1:"Mares",
+    answer2:"Sows",answer3:"Cows", answer4:"Dams", correctAnswer:"Cows"},*/
 
-    {id: cuid(), question: "Which of the following animals sleep standing up?", answer1:"Gorillas",
-    answer2:"Flamingos",answer3:"Camels", answer4:"Ravens", correctAnswer:"Flamingos"},
+    {question: "Which of the following animals sleep standing up?", answers:["Gorillas","Flamingos","Camels","Ravens"], correctAnswer:1, userAnswer:0},
 
-    {id: cuid(), question: "What is the fastest water animal?", answer1:"Porpoise",
-    answer2:"Sailfish",answer3:"Flying fish", answer4:"Tuna", correctAnswer:"Sailfish"}
-
-
+    {question: "What is the fastest water animal?", answers:["Porpoise","Sailfish","Flying fish","Tuna"],
+    correctAnswer:1, userAnswer:0}
 ]
 
-function generateItemElement(item) {
-    return ` 
-        <li data-item-id="${item.id}">
-            <p class="question">${item.question}</p>
-            <div class="shopping-item-controls">
-                <button>
-                    <span>${answer1}</span>
-                </button>
-                <button>
-                    <span>${answer2}</span>
-                </button>
-                <button>
-                    <span>${answer3}</span>
-                </button>
-                <button>
-                    <span>${answer4}</span>
-                </button>
-            </div>
-        </li> `
+let currentQuestion = 0;
 
+
+function generateAnswerHtml(item,questionIndex) {
+    return item.answers.map( (answer,index) => 
+        `<div class="question">
+        <form action = "quizResultsPage.html">
+            <button id = ${index} data-Question= ${questionIndex} class="selection-Button Answer">
+                <span>${answer}</span>
+            </button>
+        </form>
+    </div>`).join("")
+}
+
+function generateItemElement(item,index) {
+
+    return `
+    
+
+        <p class="question-Sentence">${item.question}</p>
+        ${generateAnswerHtml(item,index)}
+    
+    `
     //generates the html
 
 }
 
 function generateFullQuestionList(questionList) {
 
-    const questions = questionList.map((item) => generateItemElement(item));
+    const item = questionList.map((item,index) => generateItemElement(item,index));
 
     return item.join("");
 
@@ -70,15 +70,26 @@ function renderQuestions() {
 
     const questionString = generateFullQuestionList(questionAndAnswer);
 
-    $('.question-List').html(questionString);
+    $('.question-Block').html(questionString);
+
+    $('.selection-Button.Answer').on('click', event => {
+
+        event.preventDefault();
+
+        const question = $(event.currentTarget).data("question"); 
+
+        questionAndAnswer[question].userAnswer = $(event.currentTarget).attr("id");
+
+        currentQuestion++;
+    });
 
     //renders the list of questions and answers
 
 }
 
-function getItemIdForQuestionItem() {
+function getItemAnswer() {
 
-    //gets the id from the question they chose an answer for
+    //gets the answer from the question they chose an answer for
 
 }
 
