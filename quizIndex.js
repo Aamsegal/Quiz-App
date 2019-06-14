e86aab6860557ba5487c091a99e09b55fbc08687
Amsegal93 <Amsegal93@gmail.com>
2019-06-14 11:44:23 -0400

    Added results page generation. Shows correct answer, the answer the user selected and if the answer is correct. Then shows how many answers the user got correct

--- quizIndex.js
+++ quizIndex.js
@@ -4,7 +4,7 @@ const questionAndAnswer = [
     {question: "Which of the following dogs is the smallest", answers:["Dachshund","Poodle","Pomeranian","Chihuahua"], correctAnswer:3, userAnswer:4},
 
     {question: "What color are Zebras?", answers:["White with black stripes","Black with shite stripes","Both of the above","None of the above"], 
-    correctAnswer:2, userAnswer:4},
+    correctAnswer:1, userAnswer:4},
 
     {question: "What existing bird has the largest wingspan", answers:["Stork","Swan","Condor","Albatross"], correctAnswer:3, userAnswer:4},
 
@@ -23,6 +23,8 @@ const questionAndAnswer = [
 ]
 let currentQuestion = 0;
 //value to identify what question the user is currently on
+
+let correctAnswers = 0;
 
 let userSelectedAnswer = [4,4,4,4,4,4,4,4,4,4]
 /*create an array of the users answeres. If one of the elements in the array is 4 when they try to move on
@@ -48,25 +50,14 @@ function generateItemElement(item,index) {
         <p class="question-Sentence">${item.question}</p>
         ${generateAnswerHtml(item,index)}
          `
+         
     }
     
     //generates html for each question. If they are on the last question, also add the submit botton
 
-function generateFullQuestionList(questionList) {
-
-    const item = questionList.map((item,index) => generateItemElement(item,index));
-
-    return item.join("");
-
-    //generates the html for each question
-
-}
-
 function renderQuestions() {
 
-    /*const questionString = generateFullQuestionList(questionAndAnswer);*/
-
-    const questionString = generateItemElement(questionAndAnswer[currentQuestion]);
+    const questionString = generateItemElement(questionAndAnswer[currentQuestion],currentQuestion);
 
     $('.question-Block').html(questionString);
 
@@ -74,26 +65,74 @@ function renderQuestions() {
 
 }
 
-/*function submitButtonPress() {
 
-        if (userSelectedAnswer.includes(3) === true) {
-            console.log('The array has a 4 in it');
-        }
-        
-        else {
-            document.getElementById('show-1').style.display = "none";
-            document.getElementById('show-2').style.display = "none";
 
-            document.getElementById('hidden-1').style.display = "block";
-            document.getElementById('hidden-2').style.display = "block";
-        }
+
+function generateCorrectAnswerHtml(item) {
+    const answer = item.answers[item.correctAnswer];
+    return `<div class = "question-Block">
+        <h2>Correct Answer</h2>
+        <div class="question">
+            <p class="results-Choice">${answer}</p>
+        </div>`
+}
+//Generates html for every answer in the questionAndAnswer variable
+
+function generateUserAnswerHtml(item,questionIndex) {
+
+    const userAnswerIndex = userSelectedAnswer[questionIndex];
+    const userAnswer = item.answers[userAnswerIndex];
+    const answer = item.correctAnswer;
+    
+    if(userAnswerIndex === answer) {
+
+        return `<h2>Your Answer</h2>
+            <div class="question">
+                <p class="results-Choice correct-Answer">${userAnswer}</p>
+            </div>
+        </div>`
+
+    } else {
+        return `<h2>Your Answer</h2>
+            <div class="question">
+                <p class="results-Choice incorrect-Answer">${userAnswer}</p>
+            </div>
+        </div>`
     }
-    If the button is clicked, and the user has not selected an answer for 
-    one of the questions, text will display on the screen saying they missed
-    an answer*/
 
-function renderResultsPage() {
-    //renders the results page
+        
+}
+
+function generateResultElement(item,index) {
+
+        return `
+
+        <p class="question-Sentence">${item.question}</p>
+        ${generateCorrectAnswerHtml(item,index)}
+        ${generateUserAnswerHtml(item,index)}
+         `
+    }
+
+
+function generateResultQuestionList(questionList) {
+
+    const item = questionList.map((item,index) => generateResultElement(item,index));
+
+    return item.join("");
+
+    //generates the html for each question
+
+}
+
+function renderResults() {
+
+    const questionString = generateResultQuestionList(questionAndAnswer);
+
+
+    $('.report-Block').html(questionString);
+
+    //renders the list of questions and answers
+
 }
 
 function answerSelection() {
@@ -101,15 +140,25 @@ function answerSelection() {
     $('.selection-Button.Answer').on('click', event => {
 
         if (currentQuestion === 9) {
+            event.preventDefault();
 
-            console.log(userSelectedAnswer);
+            const answerIndex = $(event.currentTarget).val();
+
+            let questionAnswerIndex = $(event.currentTarget).attr("data-Question");
+
+            userSelectedAnswer[questionAnswerIndex] = Number(answerIndex);
+
+            if (userSelectedAnswer[questionAnswerIndex] === questionAndAnswer[currentQuestion].correctAnswer) {
+                correctAnswers++;
+            }   //if the user selects the correct answer, increase the correct answer count by 1
 
             event.preventDefault();
 
-            document.getElementById('show-1').style.display = "none";
-            document.getElementById('show-2').style.display = "none";
+            $("#show-1").hide();
+            $("#show-2").hide();
 
-            generateResultHtml(item,questionIndex);
+            score();
+            renderResults();
 
             document.getElementById('hidden-1').style.display = "block";
             document.getElementById('hidden-2').style.display = "block";
@@ -125,16 +174,18 @@ function answerSelection() {
 
             userSelectedAnswer[questionAnswerIndex] = Number(answerIndex);
             
+            if (userSelectedAnswer[questionAnswerIndex] === questionAndAnswer[currentQuestion].correctAnswer) {
+                correctAnswers++;
+            }   //if the user selects the correct answer, increase the correct answer count by 1
+
             currentQuestion++;
 
-            console.log(answerIndex);  
-            console.log(questionAnswerIndex);  
-            console.log(userSelectedAnswer);          
-            console.log(currentQuestion); 
+            console.log(correctAnswers);
 
             
 
             renderQuestions();
+            answerSelection();
         }
     });
     /*
@@ -146,33 +197,15 @@ function answerSelection() {
     */
 }
 
-/*function generateResultHtml(item,questionIndex) {
-    return item.answers.map( (answer,index) => 
-        `<div class="question">
-        <form action = "quizResultsPage.html">
-            <button id = ${index} data-Question= ${questionIndex} class="selection-Button Answer" value = ${index}>
-                <span>${answer}</span>
-            </button>
-        </form>
-    </div>`).join("")
-}*/
-
-function handleQuestionAnswerSelected() {
-
-    //handles when the user chooses one of the answers
-
-}
-
-function isAnswerCorrect() {
-
-    //checks if the answer is correct and act accourdingly
-
+function score() {
+    const scoreText = `You got ${correctAnswers}/10.`;
+    $('.score').html(scoreText);
 }
 
 function initialFunctionCall() {
     renderQuestions();
     answerSelection();
-    renderResultsPage();
+    /*renderResultsPage();*/
     //calls all the functions previously mentions, allowing for interaction on the web app
 
 }
