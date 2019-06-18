8cf51e479a5746783b2b46a2291d7f44a3e2a62a
Amsegal93 <Amsegal93@gmail.com>
2019-06-18 03:52:12 -0400

    Added live question and correct answer tracking as well as improved semantic html

--- quizIndex.js
+++ quizIndex.js
@@ -1,18 +1,18 @@
 const questionAndAnswer = [
     {question: "What type of animal is a seahorse?", answers:["Crustacean", "Arachnid","Fish","Shell"], correctAnswer: 0, userAnswer: 4},
 
-    {question: "Which of the following dogs is the smallest", answers:["Dachshund","Poodle","Pomeranian","Chihuahua"], correctAnswer:3, userAnswer:4},
+    {question: "Which of the following dogs is the smallest?", answers:["Dachshund","Poodle","Pomeranian","Chihuahua"], correctAnswer:3, userAnswer:4},
 
     {question: "What color are Zebras?", answers:["White with black stripes","Black with shite stripes","Both of the above","None of the above"], 
     correctAnswer:1, userAnswer:4},
 
-    {question: "What existing bird has the largest wingspan", answers:["Stork","Swan","Condor","Albatross"], correctAnswer:3, userAnswer:4},
+    {question: "What existing bird has the largest wingspan?", answers:["Stork","Swan","Condor","Albatross"], correctAnswer:3, userAnswer:4},
 
-    {question: "Whats the biggest animal that has ever lived", answers:["Blue whale","African elephant","Brontosaurus","Spinosaurus"], correctAnswer:0, userAnswer:4},
+    {question: "Whats the biggest animal that has ever lived?", answers:["Blue whale","African elephant","Brontosaurus","Spinosaurus"], correctAnswer:0, userAnswer:4},
 
-    {question: "What pets do more familes own", answers:["Birds","Cats","Dogs","Horses"], correctAnswer:1, userAnswer:4},
+    {question: "What pets do more familes own?", answers:["Birds","Cats","Dogs","Horses"], correctAnswer:1, userAnswer:4},
 
-    {question: "What animal lives the longest", answers:["Clam","Red sea urchin","Galapagos tortois","Rougheye rockfish"], correctAnswer:0, userAnswer:4},
+    {question: "What animal lives the longest?", answers:["Clam","Red sea urchin","Galapagos tortois","Rougheye rockfish"], correctAnswer:0, userAnswer:4},
 
     {question: "What are female elephants called?", answers:["Mares","Sows","Cows","Dams"], correctAnswer:2, userAnswer:4},
 
@@ -33,13 +33,11 @@ they will be prompted to answer the question before they can move on*/
 
 function generateAnswerHtml(item,questionIndex) {
     return item.answers.map( (answer,index) => 
-        `<div class="question">
-        <form>
+        `<div class="question" role="question-Block">
             <button id=${index} data-Question=${questionIndex} class="selection-Button Answer" value=${index}>
                 <span>${answer}</span>
             </button>
-        </form>
-    </div>`).join("")
+            </div>`).join("")
 }
 //Generates html for every answer in the questionAndAnswer variable
 
@@ -47,22 +45,44 @@ function generateItemElement(item,index) {
 
         return `
 
-        <p class="question-Sentence">${item.question}</p>
+        <legend class="question-Sentence">${item.question}</legend>
+        <form>
         ${generateAnswerHtml(item,index)}
+        </form>
          `
-         
+        //generates html for each question. If they are on the last question, also add the submit botton
     }
     
-    //generates html for each question. If they are on the last question, also add the submit botton
+    
+
+function generateQuestionText() {
+    const questionNumber = currentQuestion+1;
+    return `<header class="question-Sentence">Question ${questionNumber}/10</header>`
+     //generates html to show what question the user is on
+}
+
+function generateCorrectAnswerRatio() {
+    const questionNumber = currentQuestion;
+    return `<header class="question-Sentence">${correctAnswers}/${questionNumber} correct.</header>`
+    //generates html for how many questions to got correct
+}
 
 function renderQuestions() {
 
-    const questionString = generateItemElement(questionAndAnswer[currentQuestion],currentQuestion);
+    const questionString = generateQuestionText(questionAndAnswer[currentQuestion],currentQuestion);
+    const answersString = generateItemElement(questionAndAnswer[currentQuestion],currentQuestion);
+    const correctString = generateCorrectAnswerRatio(questionAndAnswer[currentQuestion],currentQuestion);
+    
 
-    $('.question-Block').html(questionString);
+    $('.question-Text').html(questionString);
+    $('.question-Block').html(answersString);
+    if (currentQuestion > 0) {
+        $('.current-Score').html(correctString);
+    }
 
-    //renders the list of questions and answers
 
+    /*calls the functions that generate html for what number question they are on, the question information and the ration
+    of correct questions to incorrect questions*/
 }
 
 
@@ -70,9 +90,9 @@ function renderQuestions() {
 
 function generateCorrectAnswerHtml(item) {
     const answer = item.answers[item.correctAnswer];
-    return `<div class = "question-Block">
+    return `<div class = "question-Block" role="question-Block-Container">
         <h2>Correct Answer</h2>
-        <div class="question">
+        <div class="question" role="question-Block">
             <p class="results-Choice">${answer}</p>
         </div>`
 }
@@ -87,14 +107,14 @@ function generateUserAnswerHtml(item,questionIndex) {
     if(userAnswerIndex === answer) {
 
         return `<h2>Your Answer</h2>
-            <div class="question">
+            <div class="question" role="question-Block">
                 <p class="results-Choice correct-Answer">${userAnswer}</p>
             </div>
         </div>`
 
     } else {
         return `<h2>Your Answer</h2>
-            <div class="question">
+            <div class="question" role="question-Block">
                 <p class="results-Choice incorrect-Answer">${userAnswer}</p>
             </div>
         </div>`
@@ -105,13 +125,14 @@ function generateUserAnswerHtml(item,questionIndex) {
 
 function generateResultElement(item,index) {
 
-        return `
 
-        <p class="question-Sentence">${item.question}</p>
-        ${generateCorrectAnswerHtml(item,index)}
-        ${generateUserAnswerHtml(item,index)}
-         `
-    }
+    return `
+
+    <p class="question-Sentence">${item.question}</p>
+    ${generateCorrectAnswerHtml(item,index)}
+    ${generateUserAnswerHtml(item,index)}
+    `
+}
 
 
 function generateResultQuestionList(questionList) {
@@ -128,7 +149,7 @@ function renderResults() {
 
     const questionString = generateResultQuestionList(questionAndAnswer);
 
-
+    
     $('.report-Block').html(questionString);
 
     //renders the list of questions and answers
@@ -156,6 +177,7 @@ function answerSelection() {
 
             $("#show-1").hide();
             $("#show-2").hide();
+            $("#show-3").hide();
 
             score();
             renderResults();
@@ -174,18 +196,15 @@ function answerSelection() {
 
             userSelectedAnswer[questionAnswerIndex] = Number(answerIndex);
             
-            if (userSelectedAnswer[questionAnswerIndex] === questionAndAnswer[currentQuestion].correctAnswer) {
-                correctAnswers++;
-            }   //if the user selects the correct answer, increase the correct answer count by 1
-
-            currentQuestion++;
-
-            console.log(correctAnswers);
-
+            const isCorrectAnswer = userSelectedAnswer[questionAnswerIndex] === questionAndAnswer[currentQuestion].correctAnswer;
             
+            renderFeedback(isCorrectAnswer);
+        
+            
+            /*renderQuestions();
+            answerSelection();*/
 
-            renderQuestions();
-            answerSelection();
+
         }
     });
     /*
@@ -197,14 +216,67 @@ function answerSelection() {
     */
 }
 
+function renderFeedback(isCorrectAnswer) {
+    $("#show-1").hide();
+    $("#show-2").hide();
+    $("#show-3").hide();
+
+    $("#feedback").show();
+
+    let feedbackText = 'placeholder';
+
+    if (isCorrectAnswer) {
+        feedbackText = `<p> Correct!</> 
+        <form>
+            <button class="feedback-Button">
+                <span>Next</span>
+            </button>
+        </form>`
+
+        correctAnswers++;
+        currentQuestion++;
+    } else {
+        feedbackText = `<p> Incorrect</> 
+        <form>
+            <button class="feedback-Button">
+                <span>Next</span>
+            </button>
+        </form>`
+        currentQuestion++;
+    }
+    
+    $('.feedback').html(feedbackText);
+
+    renderQuestions();
+    answerSelection();
+    nextQuestion();
+}
+
+function nextQuestion() {
+    $('.feedback-Button').on('click', event => {
+        event.preventDefault();
+
+        $("#feedback").hide();
+
+        $("#show-1").show();
+        $("#show-2").show();
+        $("#show-3").show();
+
+        renderQuestions();
+        answerSelection();
+    });
+}
+
 function score() {
-    const scoreText = `You got ${correctAnswers}/10.`;
+    const scoreText = `<p>You got ${correctAnswers}/10 correct.</p>`;
     $('.score').html(scoreText);
 }
 
 function initialFunctionCall() {
     renderQuestions();
     answerSelection();
+    $("#feedback").hide();
+
     /*renderResultsPage();*/
     //calls all the functions previously mentions, allowing for interaction on the web app
 
